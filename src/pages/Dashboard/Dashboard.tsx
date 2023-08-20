82356f957ec5463eef042865f3a816a4881a180c
nɒvƎ ɿɘiɿʜɒʜƧ ʇɘtA <91305837+atefevan@users.noreply.github.com>
2023-08-20 08:24:10 +0600

    u3

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,23 @@
-import Batches from "../Batches/Batches";
-import Departments from "../Departments/Departments";
+import { Box, Typography } from "@mui/material";
 import Info from "../Info/Info";
+import Departments from "../Departments/Departments";
+import Batches from "../Batches/Batches";
 
 const Dashboard = () => {
+  const dept = "CSE";
+  const batches = ["55", "51", "52", "53"];
   return (
     <>
-      {/* <Departments />
-      <Batches /> */}
-      <Info />
+      <Departments />
+      <Batches />
+      <Box>
+        <Typography variant="body1" fontWeight={600} sx={{ m: 4 }}>
+          Department ➨ {dept}
+        </Typography>
+        {batches.map((e) => (
+          <Info batch={e} />
+        ))}
+      </Box>
     </>
   );
 };
--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,7 +1,18 @@
-import { Box, Typography } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+interface Props {
+  batch: string;
+}
 
-const Info = () => {
-  const dept = "CSE";
+const Info = ({ batch }: Props) => {
+  const cse_55 = "55A";
+  const date = useState<Date>();
+  const c_start = useState();
+  const c_end = useState();
+  const c_subject = useState();
+  const c_code = useState();
+  const c_room = useState();
+  const c_teacher_initial = useState();
   return (
     <>
       <Box
@@ -10,26 +21,124 @@ const Info = () => {
           right: 0,
           top: 0,
           bottom: 0,
-          position: "absolute",
+          height: "50vh",
+          mt: 10,
         }}
       >
-        <Box>
-          <Typography variant="body1" fontWeight={600} sx={{ m: 4 }}>
-            Department ➨ {dept}
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            border: 2,
-            borderRadius: 2,
-            borderColor: "black",
-            height: "50%",
-            mr: 4,
-            ml: 4,
-          }}
-        >
-            
-        </Box>
+        <div style={{ display: "flex" }}>
+          <Box>
+            <Typography
+              variant="h3"
+              fontWeight={300}
+              sx={{ mr: 4, ml: 4, mt: 20 }}
+            >
+              {batch}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              border: 2,
+              borderWidth: 1,
+              borderColor: "black",
+              mr: 5,
+            }}
+          />
+          <Box
+            sx={{
+              height: "55%",
+              display: "flex",
+              width:"100vw",
+              flex:1,
+              m: 1,
+            }}
+          >
+            <div
+              style={{
+                justifyContent: "space-between",
+                display: "flex",
+                flexDirection: "column",
+                margin: 1,
+              }}
+            >
+              <input type="date" style={{ margin: 3 }} name="date" />
+
+              <TextField type="text" sx={{ m: 1 }} label={"class start"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class end"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class subject"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class code"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class room"} />
+              <TextField
+                type="text"
+                sx={{ m: 1 }}
+                label={"class teacher initial"}
+              />
+            </div>
+            <div
+              style={{
+                justifyContent: "space-between",
+                display: "flex",
+                flexDirection: "column",
+                margin: 1,
+              }}
+            >
+              <input type="date" style={{ margin: 3 }} name="date" />
+
+              <TextField type="text" sx={{ m: 1 }} label={"class start"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class end"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class subject"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class code"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class room"} />
+              <TextField
+                type="text"
+                sx={{ m: 1 }}
+                label={"class teacher initial"}
+              />
+            </div>
+            <div
+              style={{
+                justifyContent: "space-between",
+                display: "flex",
+                flexDirection: "column",
+                margin: 1,
+              }}
+            >
+              <input type="date" style={{ margin: 3 }} name="date" />
+
+              <TextField type="text" sx={{ m: 1 }} label={"class start"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class end"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class subject"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class code"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class room"} />
+              <TextField
+                type="text"
+                sx={{ m: 1 }}
+                label={"class teacher initial"}
+              />
+            </div>
+            <div
+              style={{
+                justifyContent: "space-between",
+                display: "flex",
+                flexDirection: "column",
+                margin: 1,
+              }}
+            >
+              <input type="date" style={{ margin: 3 }} name="date" />
+
+              <TextField type="text" sx={{ m: 1 }} label={"class start"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class end"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class subject"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class code"} />
+              <TextField type="text" sx={{ m: 1 }} label={"class room"} />
+              <TextField
+                type="text"
+                sx={{ m: 1 }}
+                label={"class teacher initial"}
+              />
+            </div>
+          </Box>
+        </div>
+        <Box sx={{ border: 2, borderWidth: 1, borderColor: "black" }} />
       </Box>
     </>
   );
